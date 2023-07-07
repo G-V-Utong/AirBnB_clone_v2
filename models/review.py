56269fad20a08ecc4b8c67ca9020701ba08e9eb2
Godswill Utong <94043309+G-V-Utong@users.noreply.github.com>
2023-07-07 16:26:11 +0100

    Initial commit

---
+++ models/review.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+""" Review module for the HBNB project """
+from sqlalchemy.sql.schema import ForeignKey
+from models.base_model import BaseModel, Base
+from sqlalchemy import Column, String, ForeignKey
+from models import storage_type
+
+
+class Review(BaseModel, Base):
+    """ Review classto store review information """
+    if storage_type == 'db':
+
+        __tablename__ = 'reviews'
+        text = Column(String(1024), nullable=False)
+        place_id = Column(String(60), ForeignKey('places.id'), nullable=False)
+        user_id = Column(String(60), ForeignKey('users.id'), nullable=False)
+    else:
+        place_id = ""
+        user_id = ""
+        text = ""
+
+    def __init__(self, *args, **kwargs):
+        """initializes Review"""
+        super().__init__(*args, **kwargs)
